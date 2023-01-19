943b4db512239d8ed418056131a7a50b5c066e44
Tushar Bhutani <tubhutani13@gmail.com>
2023-01-19 22:31:44 +0530

    fixed helper and view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,10 @@
-def user_profile_picture_link(user)
-    user.profile_picture.present? ? rails_blob_path(user.profile_picture.variant(:thumb), disposition: 'attachment') : 'default.jpeg'
-end
-def change_password
+module UsersHelper
+  def user_profile_picture_link(user)
+    user.profile_picture.present? ? rails_blob_path(user.profile_picture.variant(:thumb), disposition: "attachment") : "default.jpeg"
+  end
+
+  def change_password
     current_user.generate_change_password_token
     redirect_to edit_password_reset_url(current_user.password_reset_token)
+  end
 end
