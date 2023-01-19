92972642978669e53115159317a165d96fbfe43f
Tushar Bhutani <tubhutani13@gmail.com>
2023-01-19 16:53:46 +0530

    added Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    before_save { self.email = email.downcase }
+    validates :name, presence: true
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    validates :password, presence: true, length: { minimum: 6 }
 end
