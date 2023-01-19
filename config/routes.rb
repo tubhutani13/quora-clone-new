71212c157e14065c621368ee1f268ac8e4f3b4e9
Tushar Bhutani <tubhutani13@gmail.com>
2023-01-19 17:11:56 +0530

    added views

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+  get "/signup", to: "users#new"
+  resources :users
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
 end
