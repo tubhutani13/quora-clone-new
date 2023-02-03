1c81226ec81910e2ba55b563feccab8078a0eb80
Tushar Bhutani <tubhutani13@gmail.com>
2023-02-03 15:31:58 +0530

    controller fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,30 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:destroy]
+  before_action :load_comment, only: [:destroy]
   before_action :set_entity, only: [:create]
-  before_action :set_question
+  before_action :load_question
 
   def create
-    @comment = @entity.comments.new(comment_params)
-    @comment.user = current_user
+    @comment = @entity.comments.new(comment_params.merge(user: current_user))
     if @comment.save
-      redirect_to @question, notice: "Comment submitted successfully"
+      redirect_to @question, notice: t("comment_submit_success")
     else
-      flash[:error] = "error while creating comment"
+      flash[:error] = t("comment_publish_error")
       render "questions/show", status: :unprocessable_entity
     end
   end
 
   def destroy
-    @comment.destroy
-
-    redirect_to @question, notice: "Comment Deleted successfully"
+    if @comment.user == current_user
+      if @comment.destroy
+        redirect_to @question, notice: t("comment_delete_success")
+      else
+        flash[:error] = t("comment_delete_error")
+        render "questions/show", status: :unprocessable_entity
+      end
+    else
+      flash[:error] = t("comment_user_error")
+      render "questions/show", status: :unprocessable_entity
+    end
   end
 
   private
@@ -26,15 +33,15 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:user_id, :body, :commentable_id, :commentable_type)
   end
 
-  def set_comment
+  def load_comment
     @comment = Comment.find(params[:id])
   end
 
   def set_entity
     @entity = comment_params[:commentable_type].constantize.find(comment_params[:commentable_id])
   end
-  
-  def set_question
+
+  def load_question
     @question = Question.find_by(permalink: params[:question_permalink])
   end
 end
