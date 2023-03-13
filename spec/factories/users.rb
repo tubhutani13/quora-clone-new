5107be3014589efd4c5af11f14abd91b04fa790a
Tushar Bhutani <tubhutani13@gmail.com>
2023-03-13 15:52:53 +0530

    setup rspec

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name}
+    email { Faker::Internet.email(name: name , domain: 'quora-clone.com') }
+    password {Faker::Internet.password(min_length: 6)}
+  end
+end
--- test/channels/question_channel_test.rb
+++
@@ -1,8 +0,0 @@
-equire "test_helper"
-
-class QuestionChannelTest < ActionCable::Channel::TestCase
-  # test "subscribes" do
-  #   subscribe
-  #   assert subscription.confirmed?
-  # end
-end
